789f36a553ab3057673367eaf7a3dec8db2f80f6
alberto.riccardo.martinelli@gmail.com <martinel@di.unito.it>
2020-04-20 16:08:22 +0200

    now print_bin_file_mpi doesn't need the number or elements to read as input

--- mpi_io_examples/contiguous_write_read/print_bin_file_mpi.cpp
+++ mpi_io_examples/contiguous_write_read/print_bin_file_mpi.cpp
@@ -1,12 +1,13 @@
 #include <mpi.h>
 #include <iostream>
-int print_buf(int buf[], int size) {
+void print_buf(char buf[], int size) {
+    std::cout << "print buffer" << std::endl;
     for (int i = 0; i < size; ++i) {
         std::cout << buf[i] << std::endl;
     }
 }
 
-bool get_input(int argc, char** argv, char** path, int &size) {
+bool get_input(int argc, char** argv, char** path) {
     int my_rank, comm_world_size;
     bool result = false; ;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -16,14 +17,13 @@ bool get_input(int argc, char** argv, char** path, int &size) {
             std::cout << "error: this program must be launched with only one process" << std::endl;
         }
     }
-    else if (argc < 3) {
+    else if (argc != 2) {
         std::cout << "input error: miss some inputs" << std::endl;
-        std::cout << "inputs needed: path of the file to read and number of ints to read" << std::endl;
+        std::cout << "inputs needed: path of the file to read" << std::endl;
 
     }
     else {
         *path = argv[1];
-        size = std::stoi(argv[2]);
         result = true;
     }
     return result;
@@ -32,25 +32,30 @@ bool get_input(int argc, char** argv, char** path, int &size) {
 // reads a binary file created using MPI I/O end prints the result on terminal
 
 int main(int argc, char** argv) {
-    int* buf;
-    int size, open_result;
+    char buf[1024];
+    int open_result, num_read;
     MPI_Status status;
     MPI_File fh;
     char* path;
     MPI_Init(&argc, &argv);
-
-    if (get_input(argc, argv, &path, size)) {
-        buf = (int*) malloc(size * sizeof(int));
+    num_read = 1024;
+    for (int i = 0; i < 1024; ++i) {
+        buf[i] = 'a';
+    }
+    if (get_input(argc, argv, &path)) {
         open_result = MPI_File_open(MPI_COMM_WORLD, path, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
         if (open_result == MPI_SUCCESS) {
-            MPI_File_read(fh, buf, size, MPI_INT, &status);
-            print_buf(buf, size);
+            while (num_read == 1024) {
+                MPI_File_read(fh, buf, 1024, MPI_CHAR, &status);
+                MPI_Get_count(&status, MPI_CHAR, &num_read);
+                print_buf(buf, num_read);
+            }
+            std::cout << "num_read " << num_read << std::endl;
             MPI_File_close(&fh);
         }
         else {
             std::cout << "error path " + std::string(path) + " not valid " << std::endl;
         }
-        free(buf);
     }
 
     MPI_Finalize();
