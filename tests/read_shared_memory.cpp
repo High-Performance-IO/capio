fdde97bd9c573098499874f72eed71702f820d68
alberto.riccardo.martinelli@gmail.com <martinel@di.unito.it>
2020-04-21 08:53:58 +0200

    added test for structs

--- tests/read_shared_memory.cpp
+++ tests/read_shared_memory.cpp
@@ -2,13 +2,24 @@
 #include <cassert>
 #include "../capio.hpp"
 
+struct elem {
+    int i;
+    double d;
+    elem() {
+        i = 0;
+        d = 0;
+    }
+};
+
 /*
- * single consumer that read a sequence of integers through the capio proxy
+ * single consumer that read a sequence of integers and a sequence of structs through the capio proxy
  * to use in combination with a producer
  */
 
+int const num_writes = 45;
+
 int main(int argc, char** argv) {
-    capio_proxy<int> proxy("outputfile5", 10);
+    capio_proxy<int> proxy("outputfile3", 10);
     std::cout << "after constuctor\n";
     int j = 0;
     while (! proxy.done()) {
@@ -20,6 +31,19 @@ int main(int argc, char** argv) {
     }
     std::cout << "before finished\n";
     proxy.clean_resources();
+    capio_proxy<struct elem> proxy2("outputfile4", 12);
+    std::cout << "after constuctor\n";
+    struct elem e;
+    j = 0;
+    while (! proxy2.done()) {
+        e = proxy2.read();
+        std::cout << "read\n";
+        assert(e.i == j);
+        assert(e.d == (double) j / num_writes);
+        std::cout << e.i << " " << e.d << "\n";
+        ++j;
+    }
+    proxy2.clean_resources();
 }
 
 
--- tests/write_shared_memory.cpp
+++ tests/write_shared_memory.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include "../capio.hpp"
 
+struct elem {
+    int i;
+    double d;
+    elem() {
+        i = 0;
+        d = 0;
+    }
+};
+
 /*
- * single consumer that write a sequence of integer of fixed length using the capio proxy
+ * single producer that write a sequence of integers and a sequence of structs using the capio proxy
  * to use in combination with a consumer
  */
 
+int const num_writes = 45;
+
 int main(int argc, char** argv) {
-    capio_proxy<int> proxy("outputfile5", 10);
+    capio_proxy<int> proxy("outputfile3", 10);
     std::cout << "after constuctor\n";
-    for (int i = 0; i < 45; ++i) {
+    for (int i = 0; i < num_writes; ++i) {
         proxy.write(i);
         std::cout << "write\n";
     }
     std::cout << "before finished\n";
     proxy.finished();
-
+    capio_proxy<struct elem> proxy2("outputfile4", 12);
+    std::cout << "after constuctor\n";
+    struct elem e;
+    for (int i = 0; i < num_writes; ++i) {
+        e.i = i;
+        e.d = (double) i / num_writes;
+        proxy2.write(e);
+        std::cout << "write\n";
+    }
+    proxy2.finished();
 }
 
