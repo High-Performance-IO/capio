409a075f1df30045c6f34be1a9cadc2af0540e96
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-09-17 11:37:29 +0200

    added assert to scatter unit test

--- tests/collective/scatter/scatter_read.cpp
+++ tests/collective/scatter/scatter_read.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <mpi.h>
 #include "../../../capio_mpi/capio_mpi.hpp"
+#include "../../common/utils.hpp"
 
 int const NUM_ELEM = 100;
 
-void print_array(int data[], int array_length, int rank) {
-    std::cout << "array_length: " << array_length << std::endl;
+void compute_expected_result(int* expected_result, int array_length, int rank, int start) {
     for (int i = 0; i < array_length; ++i) {
-        std::cout << "reader " << rank << " data: " << data[i] << std::endl;
+        expected_result[i] = i + (array_length * rank) + start;
     }
 }
 
 int main(int argc, char** argv) {
-    int* data;
+    int* data, *expected_result;
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -22,11 +22,15 @@ int main(int argc, char** argv) {
     if (NUM_ELEM % size == 0) {
         int array_length = NUM_ELEM / size;
         data = new int[array_length];
+        expected_result = new int[array_length];
         capio.capio_scatter(nullptr, data, array_length);
-        print_array(data, array_length, rank);
+        compute_expected_result(expected_result, array_length, rank, 0);
+        compare_expected_actual(data, expected_result, array_length);
         capio.capio_scatter(nullptr, data, array_length);
-        print_array(data, array_length, rank);
+        compute_expected_result(expected_result, array_length, rank, 1);
+        compare_expected_actual(data, expected_result, array_length);
         free(data);
+        free(expected_result);
     }
     std::cout << "reader " << rank << " ended " << std::endl;
     MPI_Finalize();
