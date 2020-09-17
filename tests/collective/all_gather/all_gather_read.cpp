c95a7f5f675da90f4fbb96b3d1537279c8517815
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-09-17 08:54:23 +0200

    added assert in all_gather unit test

--- tests/collective/all_gather/all_gather_read.cpp
+++ tests/collective/all_gather/all_gather_read.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <assert.h>
 #include <mpi.h>
 #include "../../../capio_mpi/capio_mpi.hpp"
 
 int const NUM_ELEM = 100;
 
-void print_array(int data[], int array_length, int rank) {
-    std::cout << "array_length: " << array_length << std::endl;
+void compute_expected_result(int* expected_result, int array_length, int num_prods, int start) {
+    for (int i = 0, num = start; i < array_length; ++i) {
+       expected_result[i] = num;
+       ++num;
+       if ((i + 1) % (array_length / num_prods) == 0) {
+           ++start;
+           num = start;
+       }
+    }
+}
+
+void compare_expected_actual(int* actual, int* expected, int array_length) {
     for (int i = 0; i < array_length; ++i) {
-        std::cout << "reader " << rank << " data: " << data[i] << std::endl;
+        assert(actual[i] == expected[i]);
     }
 }
 
 int main(int argc, char** argv) {
-    int* data;
+    int* data, *expected_result;
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -28,11 +39,15 @@ int main(int argc, char** argv) {
     if (NUM_ELEM % size == 0) {
         int array_length = NUM_ELEM / size * num_prods;
         data = new int[array_length];
+        expected_result = new int[array_length];
         capio.capio_all_gather(nullptr, 0, data, array_length);
-        print_array(data, array_length, rank);
+        compute_expected_result(expected_result, array_length, num_prods, 0);
+        compare_expected_actual(data, expected_result, array_length);
         capio.capio_all_gather(nullptr, 0, data, array_length);
-        print_array(data, array_length, rank);
+        compute_expected_result(expected_result, array_length, num_prods, 1);
+        compare_expected_actual(data, expected_result, array_length);
         free(data);
+        free(expected_result);
     }
     std::cout << "reader " << rank << " ended " << std::endl;
     MPI_Finalize();
