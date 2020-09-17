22b6d23d0f9626c3f4b4f829459f2747ed068e0e
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-09-17 10:19:32 +0200

    added assert to broadcast unit test

--- tests/collective/broadcast/broadcast_read.cpp
+++ tests/collective/broadcast/broadcast_read.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <mpi.h>
 #include "../../../capio_mpi/capio_mpi.hpp"
+#include "../../common/utils.hpp"
 
 int const NUM_ELEM = 100;
 
-void print_array(int data[], int array_length, int rank) {
-    std::cout << "array_length: " << array_length << std::endl;
-    for (int i = 0; i < array_length; ++i) {
-        std::cout << "reader " << rank << " data: " << data[i] << std::endl;
+
+void compute_expected(int data[], int size) {
+    for (int i = 0; i < size; ++i) {
+        data[i] = i;
     }
 }
 
 int main(int argc, char** argv) {
-    int data[NUM_ELEM]{0};;
+    int data[NUM_ELEM]{0};
+    int expected_result[NUM_ELEM];
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     capio_mpi capio(size, true, rank);
     std::cout << "reader " << rank << " before created capio object" << std::endl;
+    compute_expected(expected_result, NUM_ELEM);
     capio.capio_broadcast(data, NUM_ELEM, 0);
-    print_array(data, NUM_ELEM, rank);
+    compare_expected_actual(data, expected_result, NUM_ELEM);
     capio.capio_broadcast(data, NUM_ELEM, 0);
-    print_array(data, NUM_ELEM, rank);
+    compare_expected_actual(data, expected_result, NUM_ELEM);
     std::cout << "reader " << rank << " ended " << std::endl;
     MPI_Finalize();
 }
