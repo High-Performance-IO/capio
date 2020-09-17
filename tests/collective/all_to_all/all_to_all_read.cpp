173ed9f4ebf916793b31b36fde1c340ba9f3c6c8
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-09-17 09:37:02 +0200

    added assert in all_to_all unit test

--- tests/collective/all_to_all/all_to_all_read.cpp
+++ tests/collective/all_to_all/all_to_all_read.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <mpi.h>
 #include "../../../capio_mpi/capio_mpi.hpp"
+#include "../../common/utils.hpp"
 
 int const NUM_ELEM = 100;
 
-void print_array(int data[], int array_length, int rank) {
-    std::cout << "array_length: " << array_length << std::endl;
-    for (int i = 0; i < array_length; ++i) {
-        std::cout << "reader " << rank << " data: " << data[i] << std::endl;
+void compute_expected_result(int* expected_result, int array_length, int size, int rank) {
+    int start = array_length / size * rank;
+    for (int i = 0, num = start; i < array_length; ++i) {
+        expected_result[i] = num;
+        ++num;
+        if ((i + 1) % (array_length / size) == 0) {
+            ++start;
+            num = start;
+        }
     }
 }
 
 int main(int argc, char** argv) {
-    int* data;
+    int* data, *expected_result;
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -26,11 +32,14 @@ int main(int argc, char** argv) {
     capio_mpi capio(size, true, rank);
     std::cout << "reader " << rank << " before created capio object" << std::endl;
     data = new int[NUM_ELEM];
+    expected_result = new int[NUM_ELEM];
     capio.capio_all_to_all(nullptr, NUM_ELEM / size, data, num_prods);
-    print_array(data, NUM_ELEM, rank);
+    compute_expected_result(expected_result, NUM_ELEM, size, rank);
+    compare_expected_actual(data, expected_result, NUM_ELEM);
     capio.capio_all_to_all(nullptr, NUM_ELEM / size, data, num_prods);
-    print_array(data, NUM_ELEM, rank);
+    compare_expected_actual(data, expected_result, NUM_ELEM);
     free(data);
+    free(expected_result);
     std::cout << "reader " << rank << " ended " << std::endl;
     MPI_Finalize();
 }
