254db1035c231092b14f0adb0f6e6968425a1954
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-09-24 18:45:21 +0200

    added unit tests scripts

---
+++ scripts/run_all_reduce_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mpiexec -n 2 ../build/capio_process/capio 2 ../one_node_2_2.yaml > all_reduce_2_2_capio.txt &
+sleep .1
+mpiexec -n 2 ../build/tests/collective/all_reduce/all_reduce_read 2 ../one_node_2_2.yaml > all_reduce_2_2_cons.txt &
+sleep .1
+mpiexec -n 2 ../build/tests/collective/all_reduce/all_reduce_write 2 ../one_node_2_2.yaml > all_reduce_2_2_prods.txt
+if [ $? -ne 0 ];
+then
+    echo "all_reduce test case 1 failed"
+fi
+
+mpiexec -n 2 ../build/capio_process/capio 4 ../one_node_2_4.yaml > all_reduce_2_4_capio.txt &
+sleep .1
+mpiexec -n 4 ../build/tests/collective/all_reduce/all_reduce_read 2 ../one_node_2_4.yaml > all_reduce_2_4_cons.txt &
+sleep .1
+mpiexec -n 2 ../build/tests/collective/all_reduce/all_reduce_write 4 ../one_node_2_4.yaml > all_reduce_2_4_prods.txt
+if [ $? -ne 0 ];
+then
+    echo "all_reduce test case 2 failed"
+fi
+
+mpiexec -n 2 ../build/capio_process/capio 2 ../one_node_4_2.yaml > all_reduce_4_2_capio.txt &
+sleep .1
+mpiexec -n 2 ../build/tests/collective/all_reduce/all_reduce_read 4 ../one_node_4_2.yaml > all_reduce_4_2_cons.txt &
+sleep .1
+mpiexec -n 4 ../build/tests/collective/all_reduce/all_reduce_write 2 ../one_node_4_2.yaml > all_reduce_4_2_prods.txt
+if [ $? -ne 0 ];
+then
+    echo "all_reduce test case 3 failed"
+fi
---
+++ scripts/run_collective_unit_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./run_all_gather_tests.sh
+./run_all_reduce_tests.sh
+./run_all_to_all_tests.sh
+./run_broadcast_tests.sh
+./run_all_gather_tests.sh
+./run_reduce_tests.sh
+./run_scatter_tests.sh
