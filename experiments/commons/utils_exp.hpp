04c312687a6465c1b4c529d076425be64b64883a
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2021-06-08 13:33:46 +0200

    added cmake support

---
+++ experiments/commons/utils_exp.hpp
@@ -0,0 +1,23 @@
+
+#ifndef CAPIO_UTILS_EXP_H
+#define CAPIO_UTILS_EXP_H
+
+int **alloc_2d_int(int rows, int cols) {
+    int *data = (int *)malloc(rows*cols*sizeof(int));
+    int **array= (int **)malloc(rows*sizeof(int*));
+    for (int i=0; i<rows; i++)
+        array[i] = &(data[cols*i]);
+
+    return array;
+}
+
+void sum(void* input_data, void* output_data, int* count, MPI_Datatype* data_type) {
+    int* input = (int*)input_data;
+    int* output = (int*)output_data;
+
+    for(int i = 0; i < *count; i++) {
+        output[i] += input[i];
+    }
+}
+
+#endif
