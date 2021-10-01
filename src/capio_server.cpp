401301875104319d23f8de1d02da20347a3b9020
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2021-10-01 15:28:01 +0200

    read after write on different machines

--- src/capio_server.cpp
+++ src/capio_server.cpp
@@ -31,11 +31,17 @@ std::unordered_map<int, std::unordered_map<int, std::string>> processes_files_me
 // pid -> (response shared buffer, index)
 std::unordered_map<int, std::pair<void*, int>> response_buffers;
 
-// pathname -> file_size
-std::unordered_map<std::string, int> files_metadata;
+// pathname -> (file_shm, file_size)
+std::unordered_map<std::string, std::pair<void*, int*>> files_metadata;
 
 // pathname -> node
 std::unordered_map<std::string, char*> files_location;
+
+// node -> rank
+std::unordered_map<std::string, int> nodes_helper_rank;
+
+// path -> (pid, fd, numbytes)
+std::unordered_map<std::string, std::tuple<int, int, int>>  remote_pending_reads;
 
 //name of the node
 
@@ -76,6 +82,11 @@ void* get_shm(std::string shm_name) {
 	return p;
 }
 
+//TODO: same function in capio_posix, to put in common
+sem_t* get_sem_requests() {
+	return sem_open("sem_requests", 0);
+}
+
 void* create_shm(std::string shm_name) {
 	void* p = nullptr;
 	// if we are not creating a new object, mode is equals to 0
@@ -92,6 +103,33 @@ void* create_shm(std::string shm_name) {
 //	if (close(fd) == -1);
 //		err_exit("close");
 	return p;
+}
+int* create_shm_int(std::string shm_name) {
+	void* p = nullptr;
+	// if we are not creating a new object, mode is equals to 0
+	int fd = shm_open(shm_name.c_str(), O_CREAT | O_RDWR,  S_IRUSR | S_IWUSR); //to be closed
+	struct stat sb;
+	const int size = sizeof(int);
+	if (fd == -1)
+		err_exit("shm_open");
+	if (ftruncate(fd, size) == -1)
+		err_exit("ftruncate");
+	p = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (p == MAP_FAILED)
+		err_exit("mmap");
+//	if (close(fd) == -1);
+//		err_exit("close");
+	return (int*) p;
+}
+//TODO: same function in capio_posix, to put in common
+struct circular_buffer get_circular_buffer() {
+	//open shm
+	void* buf = get_shm("circular_buffer");
+	int* i = (int*) get_shm("index_buf");
+	circular_buffer br;
+	br.buf = buf;
+	br.i = i;
+	return br;
 }
 
 struct circular_buffer create_circular_buffer() {
@@ -170,7 +208,7 @@ bool check_remote_file(const std::string& file_name, int rank, std::string path_
 
     int fd; /* file descriptor to identify a file within a process */
     //fd = open(file_name.c_str(), O_RDONLY);  /* -1 signals an error */
-    fp = fopen("/etc/motd", "r");
+    fp = fopen(file_name.c_str(), "r");
 	if (fp == NULL) {
 		std::cout << "capio server " << rank << " failed to open the location file" << std::endl;
 		return false;
@@ -181,16 +219,31 @@ bool check_remote_file(const std::string& file_name, int rank, std::string path_
         close(fd);
         return false;
     }
-	char** p_tmp;
 	const char* path_to_check_cstr = path_to_check.c_str();
 	bool found = false;
     while ((read = getline(&line, &len, fp)) != -1 && !found) {
         printf("Retrieved line of length %zu:\n", read);
-		char* path = strtok_r(line, " ", p_tmp);
-		char* node_str = strtok_r(NULL, " ", p_tmp);
         printf("%s", line);
+		char path[1024]; //TODO: heap memory
+		int i = 0;
+		while(line[i] != ' ') {
+			path[i] = line[i];
+			++i;
+		}
+		path[i] = '\0';
+		//char* path = strtok_r(p_line, " ", &p_tmp);
 		std::cout << "path " << path << std::endl;
-		std::cout << "node " << node_str << std::endl;
+		//char* node_str = strtok_r(NULL, "\n", &p_tmp);
+		char node_str[1024]; //TODO: heap memory 
+		++i;
+		int j = 0;
+		while(line[i] != '\n') {
+			node_str[j] = line[i];
+			++i;
+			++j;
+		}
+		node_str[j] = '\0';
+		printf("node %s\n", node_str);
 		files_location[path_to_check] = (char*) malloc(sizeof(node_str) + 1); //TODO:free the memory
 		if (strcmp(path, path_to_check_cstr) == 0) {
 			found = true;
@@ -256,7 +309,13 @@ void read_next_msg(int rank) {
 		++response_buffers[pid].second;
 		sem_post(sems_response[pid]);
 		if (files_metadata.find(path) == files_metadata.end()) {
-			files_metadata[path] = 0;	
+			std::cout << "server " << rank << "updating file metadata for " << path << std::endl;
+			files_metadata[path].first = processes_files[pid][fd].first;	
+			files_metadata[path].second = create_shm_int(path + "_size");	
+			if (files_metadata.find(path) == files_metadata.end()) {//debug
+				std::cout << "server " << rank << " error updating" <<std ::endl;
+				exit(1);
+			}
 		}
 		processes_files_metadata[pid][fd] = path;
 	}
@@ -279,7 +338,7 @@ void read_next_msg(int rank) {
 			
 			processes_files[pid][fd].second += data_size;
 			std::string path = processes_files_metadata[pid][fd];
-			files_metadata[path] += data_size; //works only if there is only one writer at time	
+			*files_metadata[path].second += data_size; //works only if there is only one writer at time	
 			//char* tmp = (char*) malloc(data_size);
 			//memcpy(tmp, processes_files[pid][fd].first, data_size); 
 			//for (int i = 0; i < data_size; ++i) {
@@ -312,7 +371,7 @@ void read_next_msg(int rank) {
 					int* tmp = (int*) malloc(count);
 					memcpy(tmp, processes_files[pid][fd].first + processes_files[pid][fd].second, count); 
 					for (int i = 0; i < count / sizeof(int); ++i) {
-						std::cout << "tmp[i] " << tmp[i] << std::endl;
+						std::cout << "server local read tmp[i] " << tmp[i] << std::endl;
 					}
 				
 					free(tmp);
@@ -324,7 +383,18 @@ void read_next_msg(int rank) {
 					sem_post(sems_response[pid]); 
 				}
 				else {
+					const char* msg;
+					std::string str_msg;
+					std::cout << " files_location[processes_files_metadata[pid][fd]] " << files_location[processes_files_metadata[pid][fd]] << std::endl;
+					int dest = nodes_helper_rank[files_location[processes_files_metadata[pid][fd]]];
+					int offset = processes_files[pid][fd].second;
+					str_msg = "read " + processes_files_metadata[pid][fd] + " " + std::to_string(rank + 1) + " " + std::to_string(offset) + " " + std::to_string(count); 
+					msg = str_msg.c_str();
+					MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
+					printf("server msg MPI_Send %s\n", msg);
+					std::cout << "msg sent to dest " << dest << std::endl;
 					std::cout << "read remote file" << std::endl;
+					remote_pending_reads[processes_files_metadata[pid][fd]] = std::make_tuple(pid, fd, count);
 				}
 			}
 			else {
@@ -333,9 +403,39 @@ void read_next_msg(int rank) {
 					std::cout << "server handling close" << std::endl;
 				}
 				else {
-					std::cout << "error msg read" << std::endl;
-					MPI_Finalize();
-					exit(1);
+					bool is_remote_read = strncmp(str, "ream", 4) == 0;
+					if (is_remote_read) {
+						int bytes_received;
+						std::string tmpstr(str + 5);	
+						std::string path = tmpstr.substr(0, tmpstr.find(" ")); // token is "scott"
+						bytes_received = std::stoi(tmpstr.substr(path.length() + 1));
+						std::cout << "server " << rank << " path " << path << " bytes_received " << bytes_received << std::endl;
+						int pid, fd, count;
+						pid = std::get<0>(remote_pending_reads[path]);
+						fd = std::get<1>(remote_pending_reads[path]);
+						count = std::get<2>(remote_pending_reads[path]);
+						//this part is equals to the local read (TODO: function)
+						int* tmp = (int*) malloc(count);
+						memcpy(tmp, processes_files[pid][fd].first + processes_files[pid][fd].second, count); 
+						for (int i = 0; i < count / sizeof(int); ++i) {
+							std::cout << "server remote read tmp[i] " << tmp[i] << std::endl;
+						}
+				
+						free(tmp);
+						std::pair<void*, int> tmp_pair = response_buffers[pid];
+						((int*) tmp_pair.first)[tmp_pair.second] = processes_files[pid][fd].second; 
+						processes_files[pid][fd].second += count;
+						//TODO: check if there is data that can be read in the local memory file
+						++response_buffers[pid].second;
+						sem_post(sems_response[pid]); 
+						*files_metadata[path].second += bytes_received;
+
+					}
+					else {
+						std::cout << "error msg read" << std::endl;
+						MPI_Finalize();
+						exit(1);
+					}
 				}
 			}
 		}
@@ -347,6 +447,7 @@ void capio_server(int rank) {
 	buf_requests = create_circular_buffer();
 	sem_requests = create_sem_requests();
 	sem_new_msgs = sem_open("sem_new_msgs", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 0);
+	MPI_Send(NULL, 0, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
 	while(true) {
 		std::cout << "serving" << std::endl;
 		read_next_msg(rank);
@@ -356,16 +457,133 @@ void capio_server(int rank) {
 }
 
 void capio_helper(int rank) {
+	char buf_recv[2048];
+	char* buf_send;
+	MPI_Status status;
+	MPI_Recv(NULL, 0, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	buf_requests = get_circular_buffer();
+	sem_requests = get_sem_requests();
+	sem_new_msgs = sem_open("sem_new_msgs", O_RDWR);
+	while(true) {
+		std::cout << "helper" << std::endl;
+		MPI_Recv(buf_recv, 2048, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status); //receive from server
+		std::cout << "helper rank " << rank << " msg received" << buf_recv << std::endl;
+		bool remote_request_to_read = strncmp(buf_recv, "read", 4) == 0;
+		if (remote_request_to_read) {
+			char** p_tmp;
+			std::cout << "before strtok" << std::endl;
+			char path_c[512];
+			int i = 5;
+			int j = 0;
+			while (buf_recv[i] != ' ') {
+				path_c[j] = buf_recv[i];
+				++i;
+				++j;
+			}
+			path_c[j] = '\0';
+			char dest_str[128];
+			j = 0;
+			++i;
+			while (buf_recv[j] != '\0') {
+				dest_str[j] = buf_recv[i];
+				++i;
+				++j;
+			}
+			dest_str[j] = '\0';
+			int dest = std::atoi(dest_str);
+			std::cout << "helper " << rank << " path " << path_c << std::endl;
+			std::cout << "helper " << rank << " dest " << dest << std::endl;
+			//int offset; at the moment ignored
+			//int count; at the moment ignored
+			const char* s1 = "sending ";
+			const size_t len1 = strlen(s1);
+			const size_t len2 = strlen(path_c);
+			// in real code you would check for errors in malloc here
+			buf_send = (char*) malloc((len1 + len2 + 1) * sizeof(char));
+			memcpy(buf_send, s1, len1);
+			memcpy(buf_send + len1, path_c, len2 + 1); // +1 to copy the null-terminator
+		    printf("helper warning sent: %s\n", buf_send);
+			std::cout << "helper dest warning = " << dest << std::endl;
+			//send warning
+			MPI_Send(buf_send, strlen(buf_send) + 1, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
+			std::cout << "helper sending data" << std::endl;
+			free(buf_send);
+			//send data
+			void* file_shm = get_shm(path_c);
+			int* size_shm = (int*) get_shm(std::string(path_c) + "_size");
+			int* tmp = (int*) malloc(*size_shm);
+			std::cout << "helper sending " << *size_shm << " bytes" << std::endl;
+			memcpy(tmp, file_shm, *size_shm); 
+			for (int i = 0; i < *size_shm / sizeof(int); ++i) {
+				std::cout << "helper sending tmp[i] " << tmp[i] << std::endl;
+			}
+				
+			free(tmp);
+			MPI_Send(file_shm, *size_shm, MPI_BYTE, dest, 0, MPI_COMM_WORLD); 
+		}
+		else if(strncmp(buf_recv, "sending", 7) == 0) { //receiving a file
+			std::string path(buf_recv + 8);
+			void* file_shm =  get_shm(path);
+			int bytes_received;
+			int source = status.MPI_SOURCE;
+			std::cout << "helper receiving data file " << path << " from process rank " << source << std::endl;
+			MPI_Recv(file_shm, 4096, MPI_BYTE, source, 0, MPI_COMM_WORLD, &status);//TODO; 4096 should be a parameter
+			MPI_Get_count(&status, MPI_CHAR, &bytes_received);
+			bytes_received *= sizeof(char);
+			std::cout << "helper " << rank << " sending wake up call to my server" << std::endl;
+			int* tmp = (int*) malloc(bytes_received);
+			memcpy(tmp, file_shm, bytes_received); 
+			for (int i = 0; i < bytes_received / sizeof(int); ++i) {
+				std::cout << "helper receiving tmp[i] " << tmp[i] << std::endl;
+			}
+				
+						free(tmp);
+			std::string msg = "ream " + path + + " " + std::to_string(bytes_received);
+			sem_wait(sem_requests);
+			const char* c_str = msg.c_str();
+			memcpy(buf_requests.buf + *buf_requests.i, c_str, strlen(c_str) + 1);
+			std::cout << "open before response" << std::endl;
+			char tmp_str[1024];
+			printf("c_str %s, len c_str %i\n", c_str, strlen(c_str));
+			sprintf(tmp_str, "%s", (char*) buf_requests.buf + *buf_requests.i);
+			printf("add open msg sent: %s\n", tmp_str);
+			*buf_requests.i = *buf_requests.i + strlen(c_str) + 1;
+			std::cout << "*buf_requests.i == " << *buf_requests.i << std::endl;
+			sem_post(sem_requests);
+			sem_post(sem_new_msgs);
 
+		}
+		else {
+			std::cout << "helper error receiving message" << std::endl;
+		}
+	}
+}
+
+
+void handshake_servers(int rank, int size) {
+	char* buf;	
+	for (int i = 0; i < size; i += 2) {
+		if (i != rank) {
+			MPI_Send(node_name, strlen(node_name), MPI_CHAR, i, 0, MPI_COMM_WORLD); //TODO: possible deadlock
+			buf = (char*) malloc(MPI_MAX_PROCESSOR_NAME * sizeof(char));//TODO: free
+			MPI_Recv(buf, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			nodes_helper_rank[buf] = i + 1;//store the rank of the helper not the server
+		}
+	}
 }
 
 int main(int argc, char** argv) {
-	int rank, len;
+	int rank, size, len;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Get_processor_name(node_name, &len);
 	std::cout << "processor name " << node_name << std::endl;
 	if (rank % 2 == 0) {
+		handshake_servers(rank, size);
+	    for (auto const &pair: nodes_helper_rank) {
+			std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+		}
 		std::cout << "capio server, rank " << rank << std::endl;
 		capio_server(rank);
 	}
