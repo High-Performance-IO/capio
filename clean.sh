c84831654709bc64c1beaed059383fa01afeba6e
ARMartinelli <alberto.riccardo.martinelli@gmail.com>
2021-11-02 10:23:23 +0100

    added clean script

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo cleaning host $HOSTNAME
+
+rm /dev/shm/buf_response* /dev/shm/file_* /dev/shm/output_file_* /dev/shm/sem.sem_new_msgs  /dev/shm/sem.sem_requests /dev/shm/sem.sem_response* /dev/shm/circular_buffer /dev/shm/index_buf /dev/shm/vader_segment.* /dev/shm/vader_segment.*
+rm files_location.txt
+touch files_location.txt
+rm file_*
