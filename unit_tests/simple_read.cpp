5dadb3ea415f122b9a2a4a43a1c495221e7216f1
ARMartinelli <albertoriccardo.martinelli@unito.it>
2022-05-31 11:35:25 +0200

    unit tests: added the number of files option

--- unit_tests/simple_read.cpp
+++ unit_tests/simple_read.cpp
@@ -7,9 +7,9 @@
 #include <unistd.h>
 #include <mpi.h>
 
-int sum_all(int *data, long int num_elements, long int num_reads) {
+int sum_all(int *data, long int num_elements, long int num_reads, long int num_files) {
 	int sum = 0;
-	for (long int k = 0; k < num_reads; ++k) {
+	for (long int k = 0; k < num_reads * num_files; ++k) {
 		for (long int i = 0; i < num_elements; ++i) {
         	if (sum > std::numeric_limits<int>::max() - data[i + k * num_elements]) {
             	sum = 0;
@@ -23,19 +23,13 @@ int sum_all(int *data, long int num_elements, long int num_reads) {
 using cclock = std::chrono::system_clock;
 using sec = std::chrono::duration<double>;
 
-void read_from_file(const std::string file_name, int* data, long int num_elements, long int num_reads, int rank, std::string time_file) {
+void read_from_file(int* data, long int num_elements, long int num_reads, int rank, long int index) {
+	std::string file_name = "file_" + std::to_string(rank) + "_" + std::to_string(index) + ".txt";
 	int fd = open(file_name.c_str(), O_RDONLY, 0644);
 	if (fd < 0) {
 		std::cerr << "reader " << rank << " failed to open the file: " << file_name << std::endl;
 		MPI_Finalize();
 		exit(1);
-	}
-	std::ofstream file;
-	cclock::time_point before;
-	if (rank == 0) {
-		file.open(time_file, std::ios_base::app);
-		// for milliseconds, use using ms = std::chrono::duration<double, std::milli>;
-		before = cclock::now();
 	}
 	for (long int i = 0; i < num_reads; ++i) {
 		long int k = 0;
@@ -52,35 +46,47 @@ void read_from_file(const std::string file_name, int* data, long int num_element
 		}
 	}
 
+
+	if (close(fd) == -1)
+		std::cerr << "process " << rank << ", error closing the file\n";
+}
+void read_from_files(int* data, long int num_elements, long int num_reads, long int num_files, int rank, std::string time_file) {
+	std::ofstream file;
+	cclock::time_point before;
+	if (rank == 0) {
+		file.open(time_file, std::ios_base::app);
+		// for milliseconds, use using ms = std::chrono::duration<double, std::milli>;
+		before = cclock::now();
+	}
+	for (long int i = 0; i < num_files; ++i) {
+		read_from_file(data + i * num_elements * num_reads, num_elements, num_reads, rank, i);
+	}
 	MPI_Barrier(MPI_COMM_WORLD);
 	if (rank == 0) {
 		const sec duration = cclock::now() - before;
 		file << "total read time: " << duration.count() << " secs" << std::endl;
 		file.close();
 	}
-
-	if (close(fd) == -1)
-		std::cerr << "process " << rank << ", error closing the file\n";
 }
 
 int main (int argc, char** argv) {
 	int rank;
-	long int num_elements, num_reads;
+	long int num_elements, num_reads, num_files;
 	std::string time_file;
 	MPI_Init(&argc, &argv);
-	if (argc != 4) {
+	if (argc != 5) {
 		std::cerr << "simple read input error" << std::endl;
 		MPI_Finalize();
 		return 0;
 	}
 	num_elements = std::atol(argv[1]);
 	num_reads = std::atol(argv[2]);
-	time_file = argv[3];
+	num_files = std::atol(argv[3]);
+	time_file = argv[4];
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::string file_name = "file_" + std::to_string(rank) + ".txt";
-	int* data = new int[num_elements * num_reads];
-	read_from_file(file_name, data, num_elements, num_reads, rank, time_file);
-	int sum = sum_all(data, num_elements, num_reads);
+	int* data = new int[num_elements * num_reads * num_files];
+	read_from_files(data, num_elements, num_reads, num_files, rank, time_file);
+	int sum = sum_all(data, num_elements, num_reads, num_files);
 	std::cout << "process " << rank << ", sum = " << sum << "\n";
 	delete[] data;
 	MPI_Finalize();
--- unit_tests/simple_write.cpp
+++ unit_tests/simple_write.cpp
@@ -17,17 +17,9 @@ void initialize_data(int* data, long int num_elements, long int num_writes, int 
 using cclock = std::chrono::system_clock;
 using sec = std::chrono::duration<double>;
 
-void write_to_file(int* data, long int num_elements, long int num_writes, int rank, std::string time_file) {
-	std::string file_name = "file_" + std::to_string(rank) + ".txt";
+void write_to_file(int* data, long int num_elements, long int num_writes, int rank, long int index, std::string time_file) {
+	std::string file_name = "file_" + std::to_string(rank) + "_" + std::to_string(index) + ".txt";
 	int fd = open(file_name.c_str(), O_CREAT | O_WRONLY, 0644);
-	std::ofstream file;
-	cclock::time_point before;
-	if (rank == 0) {
-		file.open(time_file, std::fstream::app);
-		// for milliseconds, use using ms = std::chrono::duration<double, std::milli>;
-		before = cclock::now();
-	}
-
 	for (long int i = 0; i < num_writes; ++i) {
 		int res = 0;
 		long int k = 0;
@@ -38,33 +30,47 @@ void write_to_file(int* data, long int num_elements, long int num_writes, int ra
         	k += num_elements_written;
     	}
 	}
+	
+	if (close(fd) == -1)
+		std::cerr << "process " << rank << ", error closing the file\n";
+}
+
+void write_to_files(int* data, long int num_elements, long int num_writes, long int num_files, int rank, std::string time_file) {
+	std::ofstream file;
+	cclock::time_point before;
+	if (rank == 0) {
+		file.open(time_file, std::fstream::app);
+		// for milliseconds, use using ms = std::chrono::duration<double, std::milli>;
+		before = cclock::now();
+	}
+	for (long int i = 0; i < num_files; ++i)
+		write_to_file(data, num_elements, num_writes, rank, i, time_file);
 	MPI_Barrier(MPI_COMM_WORLD);
 	if (rank == 0) {
 		const sec duration = cclock::now() - before;
 		file << "total write time: " << duration.count() << " secs" << std::endl;
 		file.close();
 	}
-	if (close(fd) == -1)
-		std::cerr << "process " << rank << ", error closing the file\n";
 }
 
 int main (int argc, char** argv) {
 	int rank;
-	long int num_elements, num_writes;
+	long int num_elements, num_writes, num_files;
 	std::string time_file;
 	MPI_Init(&argc, &argv);
-	if (argc != 4) {
+	if (argc != 5) {
 		std::cerr << "simple write input error" << std::endl;
 		MPI_Finalize();
 		return 0;
 	}
 	num_elements = std::atol(argv[1]);
 	num_writes = std::atol(argv[2]);
-	time_file = argv[3];
+	num_files = std::atol(argv[3]);
+	time_file = argv[4];
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int* data = new int[num_elements * num_writes];
 	initialize_data(data, num_elements, num_writes, rank);
-	write_to_file(data, num_elements, num_writes, rank, time_file);
+	write_to_files(data, num_elements, num_writes, num_files, rank, time_file);
 	delete[] data;
 	MPI_Finalize();
 }
