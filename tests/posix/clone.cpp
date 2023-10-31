b725e0c5e40747ff30c031ca38f08c02907697b0
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2023-10-31 13:52:10 +0100

    Added stat tests

--- tests/posix/clone.cpp
+++ tests/posix/clone.cpp
@@ -3,8 +3,12 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <thread>
+#include <semaphore.h>
+#include <thread>
 
 constexpr int ARRAY_SIZE = 100;
+
+sem_t* sem;
 
 int func(int *num) {
     REQUIRE(*num == 12345);
@@ -17,6 +21,17 @@ void write_file(int fd) {
         array[i] = i;
     }
     REQUIRE(write(fd, array, ARRAY_SIZE * sizeof(int)) == ARRAY_SIZE * sizeof(int));
+}
+
+int write_file_stat_clone(FILE* fp) {
+    constexpr int ARRAY_SIZE = 100;
+    int array[ARRAY_SIZE];
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = i;
+    }
+    REQUIRE(fwrite(array, sizeof(int), ARRAY_SIZE, fp) == ARRAY_SIZE);
+    REQUIRE(sem_post(sem) == 0);
+    return 0;
 }
 
 TEST_CASE("Test thread clone", "[posix]") {
@@ -43,4 +58,21 @@ TEST_CASE("Test thread clone producer/consumer", "[posix]") {
     REQUIRE(read(fd, &num, sizeof(int)) == 0);
     REQUIRE(close(fd) != -1);
     REQUIRE(unlink(PATHNAME) != -1);
+}
+
+TEST_CASE("Test thread clone producer/consumer with stat", "[posix]") {
+    constexpr const char* PATHNAME = "test_file.txt";
+    sem = static_cast<sem_t*>(malloc(sizeof(sem_t)));
+    REQUIRE(sem_init(sem, 0, 0) == 0);
+    FILE* fp = fopen(PATHNAME, "w+");
+    REQUIRE(fp != nullptr);
+    std::thread t1(write_file_stat_clone, fp);
+    REQUIRE(sem_wait(sem) == 0);
+    REQUIRE(fseek(fp, 0, SEEK_SET) != -1);
+    int num;
+    while(fread(&num, sizeof(int), 1, fp) != 0);
+    REQUIRE(feof(fp) != 0);
+    REQUIRE(fclose(fp) != EOF);
+    REQUIRE(unlink(PATHNAME) != -1);
+    t1.join();
 }
--- tests/posix/stat.cpp
+++ tests/posix/stat.cpp
@@ -3,33 +3,28 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <thread>
-sem_t* sem;
+sem_t* sem1;
 
-int func(int* num) {
-    REQUIRE(*num == 12345);
-    return 0;
-}
-
-int write_file(FILE* fp) {
+int write_file_stat(FILE* fp) {
     constexpr int ARRAY_SIZE = 100;
     int array[ARRAY_SIZE];
     for (int i = 0; i < ARRAY_SIZE; i++) {
         array[i] = i;
     }
     REQUIRE(fwrite(array, sizeof(int), ARRAY_SIZE, fp) == ARRAY_SIZE);
-    REQUIRE(sem_post(sem) == 0);
+    REQUIRE(sem_post(sem1) == 0);
     return 0;
 }
 
 
-TEST_CASE("Test thread clone producer/consumer", "[posix]") {
+TEST_CASE("Test stat", "[posix]") {
     constexpr const char* PATHNAME = "test_file.txt";
-    sem = static_cast<sem_t*>(malloc(sizeof(sem_t)));
-    REQUIRE(sem_init(sem, 0, 0) == 0);
+    sem1 = static_cast<sem_t*>(malloc(sizeof(sem_t)));
+    REQUIRE(sem_init(sem1, 0, 0) == 0);
     FILE* fp = fopen(PATHNAME, "w+");
     REQUIRE(fp != nullptr);
-    write_file(fp);
-    REQUIRE(sem_wait(sem) == 0);
+    write_file_stat(fp);
+    REQUIRE(sem_wait(sem1) == 0);
     REQUIRE(fseek(fp, 0, SEEK_SET) != -1);
     int num;
     while(fread(&num, sizeof(int), 1, fp) != 0);
