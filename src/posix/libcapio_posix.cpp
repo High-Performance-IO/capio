c7ae3359b6ccff4cc2dcdf20ce9b38deb8a41790
Marco Edoardo Santimaria <santimaria@di.unito.it>
2023-12-13 20:40:02 +0100

    This commit ensures that only handlers for present syscalls are presents

--- src/posix/libcapio_posix.cpp
+++ src/posix/libcapio_posix.cpp
@@ -44,47 +44,129 @@ static constexpr std::array<CPHandler_t, __NR_syscalls> build_syscall_table() {
         _syscallTable[i] = not_handled_handler;
     }
 
-    _syscallTable[SYS_access]     = access_handler;
-    _syscallTable[SYS_chdir]      = chdir_handler;
-    _syscallTable[SYS_chmod]      = fchmod_handler;
-    _syscallTable[SYS_chown]      = fchown_handler;
-    _syscallTable[SYS_close]      = close_handler;
-    _syscallTable[SYS_creat]      = creat_handler;
-    _syscallTable[SYS_dup]        = dup_handler;
-    _syscallTable[SYS_dup2]       = dup2_handler;
-    _syscallTable[SYS_dup3]       = dup3_handler;
-    _syscallTable[SYS_execve]     = execve_handler;
-    _syscallTable[SYS_exit]       = exit_handler;
+#ifdef SYS_access
+    _syscallTable[SYS_access] = access_handler;
+#endif
+#ifdef SYS_chdir
+    _syscallTable[SYS_chdir] = chdir_handler;
+#endif
+#ifdef SYS_chmod
+    _syscallTable[SYS_chmod] = fchmod_handler;
+#endif
+#ifdef SYS_chown
+    _syscallTable[SYS_chown] = fchown_handler;
+#endif
+#ifdef SYS_close
+    _syscallTable[SYS_close] = close_handler;
+#endif
+#ifdef SYS_creat
+    _syscallTable[SYS_creat] = creat_handler;
+#endif
+#ifdef SYS_dup
+    _syscallTable[SYS_dup] = dup_handler;
+#endif
+#ifdef SYS_dup2
+    _syscallTable[SYS_dup2] = dup2_handler;
+#endif
+#ifdef SYS_dup3
+    _syscallTable[SYS_dup3] = dup3_handler;
+#endif
+#ifdef SYS_execve
+    _syscallTable[SYS_execve] = execve_handler;
+#endif
+#ifdef SYS_exit
+    _syscallTable[SYS_exit] = exit_handler;
+#endif
+#ifdef SYS_exit_group
     _syscallTable[SYS_exit_group] = exit_handler;
-    _syscallTable[SYS_faccessat]  = faccessat_handler;
+#endif
+#ifdef SYS_faccessat
+    _syscallTable[SYS_faccessat] = faccessat_handler;
+#endif
+#ifdef SYS_faccessat2
     _syscallTable[SYS_faccessat2] = faccessat_handler;
-    _syscallTable[SYS_fcntl]      = fcntl_handler;
-    _syscallTable[SYS_fgetxattr]  = fgetxattr_handler;
+#endif
+#ifdef SYS_fcntl
+    _syscallTable[SYS_fcntl] = fcntl_handler;
+#endif
+#ifdef SYS_fgetxattr
+    _syscallTable[SYS_fgetxattr] = fgetxattr_handler;
+#endif
+#ifdef SYS_flistxattr
     _syscallTable[SYS_flistxattr] = not_implemented_handler;
-    _syscallTable[SYS_fork]       = fork_handler;
-    _syscallTable[SYS_fstat]      = fstat_handler;
-    _syscallTable[SYS_fstatfs]    = fstatfs_handler;
-    _syscallTable[SYS_getcwd]     = getcwd_handler;
-    _syscallTable[SYS_getdents]   = getdents_handler;
+#endif
+#ifdef SYS_fork
+    _syscallTable[SYS_fork] = fork_handler;
+#endif
+#ifdef SYS_fstat
+    _syscallTable[SYS_fstat] = fstat_handler;
+#endif
+#ifdef SYS_fstatfs
+    _syscallTable[SYS_fstatfs] = fstatfs_handler;
+#endif
+#ifdef SYS_getcwd
+    _syscallTable[SYS_getcwd] = getcwd_handler;
+#endif
+#ifdef SYS_getdents
+    _syscallTable[SYS_getdents] = getdents_handler;
+#endif
+#ifdef SYS_getdents64
     _syscallTable[SYS_getdents64] = getdents64_handler;
-    _syscallTable[SYS_getxattr]   = not_implemented_handler;
-    _syscallTable[SYS_ioctl]      = ioctl_handler;
-    _syscallTable[SYS_lgetxattr]  = not_implemented_handler;
-    _syscallTable[SYS_lseek]      = lseek_handler;
-    _syscallTable[SYS_lstat]      = lstat_handler;
-    _syscallTable[SYS_mkdir]      = mkdir_handler;
-    _syscallTable[SYS_mkdirat]    = mkdirat_handler;
+#endif
+#ifdef SYS_getxattr
+    _syscallTable[SYS_getxattr] = not_implemented_handler;
+#endif
+#ifdef SYS_ioctl
+    _syscallTable[SYS_ioctl] = ioctl_handler;
+#endif
+#ifdef SYS_lgetxattr
+    _syscallTable[SYS_lgetxattr] = not_implemented_handler;
+#endif
+#ifdef SYS_lseek
+    _syscallTable[SYS_lseek] = lseek_handler;
+#endif
+#ifdef SYS_lstat
+    _syscallTable[SYS_lstat] = lstat_handler;
+#endif
+#ifdef SYS_mkdir
+    _syscallTable[SYS_mkdir] = mkdir_handler;
+#endif
+#ifdef SYS_mkdirat
+    _syscallTable[SYS_mkdirat] = mkdirat_handler;
+#endif
+#ifdef SYS_newfstatat
     _syscallTable[SYS_newfstatat] = fstatat_handler;
-    _syscallTable[SYS_openat]     = openat_handler;
-    _syscallTable[SYS_read]       = read_handler;
-    _syscallTable[SYS_rename]     = rename_handler;
-    _syscallTable[SYS_stat]       = lstat_handler;
-    _syscallTable[SYS_statx]      = statx_handler;
-    _syscallTable[SYS_unlink]     = unlink_handler;
-    _syscallTable[SYS_unlinkat]   = unlinkat_handler;
-    _syscallTable[SYS_write]      = write_handler;
-    _syscallTable[SYS_writev]     = writev_handler;
-    _syscallTable[SYS_rmdir]      = rmdir_handler;
+#endif
+#ifdef SYS_openat
+    _syscallTable[SYS_openat] = openat_handler;
+#endif
+#ifdef SYS_read
+    _syscallTable[SYS_read] = read_handler;
+#endif
+#ifdef SYS_rename
+    _syscallTable[SYS_rename] = rename_handler;
+#endif
+#ifdef SYS_stat
+    _syscallTable[SYS_stat] = lstat_handler;
+#endif
+#ifdef SYS_statx
+    _syscallTable[SYS_statx] = statx_handler;
+#endif
+#ifdef SYS_unlink
+    _syscallTable[SYS_unlink] = unlink_handler;
+#endif
+#ifdef SYS_unlinkat
+    _syscallTable[SYS_unlinkat] = unlinkat_handler;
+#endif
+#ifdef SYS_write
+    _syscallTable[SYS_write] = write_handler;
+#endif
+#ifdef SYS_writev
+    _syscallTable[SYS_writev] = writev_handler;
+#endif
+#ifdef SYS_rmdir
+    _syscallTable[SYS_rmdir] = rmdir_handler;
+#endif
 
     return _syscallTable;
 }
