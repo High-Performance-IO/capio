195cfe1bdc0a3cc4056850e226f9ad2c0bb58fdd
marco edoardo santimaria <marco.santimaria@edu.unito.it>
2024-07-06 09:01:45 +0000

    Test for fgetxattr

--- tests/unit/syscall/src/fcntl.cpp
+++ tests/unit/syscall/src/fcntl.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
 
 TEST(SystemCallTest, TestFchmod) {
     auto fd = open("test.txt", O_CREAT | O_RDWR, 0777);
@@ -12,6 +14,16 @@ TEST(SystemCallTest, TestFchmod) {
 TEST(SystemCallTest, TestFchown) {
     auto fd = open("test.txt", O_CREAT | O_RDWR, 0777);
     EXPECT_EQ(fchown(fd, getuid(), getgid()), 0);
+    EXPECT_EQ(close(fd), 0);
+    EXPECT_EQ(unlink("test.txt"), 0);
+}
+
+TEST(SystemCallTest, TestFgetxattr) {
+    auto fd = open("test.txt", O_CREAT | O_RDWR, 0777);
+    uid_t tmp;
+    // WARNING: CAPIO does not support yet this system call
+    //  but it need to be intercepted nevertheless. It will always return -1 as of now.
+    EXPECT_EQ(fgetxattr(fd, "system.posix_acl_access", &tmp, sizeof(uid_t)), -1);
     EXPECT_EQ(close(fd), 0);
     EXPECT_EQ(unlink("test.txt"), 0);
 }
