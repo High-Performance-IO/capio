b6d5049370d09a9983ec069008251dbffa5aec88
marco edoardo santimaria <marco.santimaria@edu.unito.it>
2024-07-06 09:42:03 +0000

    Added statfs

--- tests/unit/syscall/src/fcntl.cpp
+++ tests/unit/syscall/src/fcntl.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <fcntl.h>
+#include <sys/statfs.h>
 #include <sys/types.h>
 #include <sys/xattr.h>
 
@@ -22,10 +23,17 @@ TEST(SystemCallTest, TestFgetxattr) {
     auto fd = open("test.txt", O_CREAT | O_RDWR, 0777);
     uid_t tmp;
     // WARNING: CAPIO does not support yet this system call
-    //  but it need to be intercepted nevertheless. It will always return -1 as of now.
+    //  but it needs to be intercepted nevertheless. It will always return -1 as of now.
     EXPECT_EQ(fgetxattr(fd, "system.posix_acl_access", &tmp, sizeof(uid_t)), -1);
     EXPECT_EQ(close(fd), 0);
     EXPECT_EQ(unlink("test.txt"), 0);
 }
 
-TEST(SystemCallTest, TestStatfs) {}
+TEST(SystemCallTest, TestStatfs) {
+    struct statfs buf;
+    EXPECT_NE(statfs(".", &buf), -1);
+    auto fd = open("test.txt", O_CREAT | O_RDWR, 0777);
+    EXPECT_NE(fstatfs(fd, &buf), -1);
+    EXPECT_EQ(close(fd), 0);
+    EXPECT_EQ(unlink("test.txt"), 0);
+}
