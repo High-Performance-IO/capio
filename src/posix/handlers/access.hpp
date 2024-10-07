41e392a61fe6856c68e37d97e1e6166b75b019b4
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2024-10-07 14:38:29 +0200

    Added cache to consent to proceed handler

--- src/posix/handlers/access.hpp
+++ src/posix/handlers/access.hpp
@@ -20,7 +20,7 @@ int access_handler(long arg0, long arg1, long arg2, long arg3, long arg4, long a
         path = capio_posix_realpath(pathname);
     }
 
-    consent_to_proceed_request(path, tid, __FUNCTION__);
+    consent_request_cache->consent_request(path, tid, __FUNCTION__);
     return CAPIO_POSIX_SYSCALL_REQUEST_SKIP;
 }
 
@@ -57,7 +57,7 @@ int faccessat_handler(long arg0, long arg1, long arg2, long arg3, long arg4, lon
         }
     }
 
-    consent_to_proceed_request(path, tid, __FUNCTION__);
+     consent_request_cache->consent_request(path, tid, __FUNCTION__);
     return CAPIO_POSIX_SYSCALL_REQUEST_SKIP;
 }
 
--- src/posix/handlers/ioctl.hpp
+++ src/posix/handlers/ioctl.hpp
@@ -10,7 +10,7 @@ int ioctl_handler(long arg0, long arg1, long arg2, long arg3, long arg4, long ar
     START_LOG(tid, "call(fd=%d, request=%ld)", fd, request, tid);
 
     if (exists_capio_fd(fd)) {
-        consent_to_proceed_request(get_capio_fd_path(fd), tid, __FUNCTION__);
+        consent_request_cache->consent_request(get_capio_fd_path(fd), tid, __FUNCTION__);
     }
     return CAPIO_POSIX_SYSCALL_REQUEST_SKIP;
 }
