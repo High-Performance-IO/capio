95a258d86f0b77ba7d258e4bcaa2804927cd73bf
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2024-10-10 10:38:21 +0200

    added cache to commit files

--- src/server/client-manager/handlers/close.hpp
+++ src/server/client-manager/handlers/close.hpp
@@ -29,12 +29,14 @@ inline void close_handler(const char *const str) {
         capio_cl_engine->isProducer(filename, tid)) {
         file_manager->setCommitted(path);
 
-        // The increase close count is called only on explicit close() sc, as defined by the
-        // CAPIO-CL specification. If it were to be called every time the file is committed, then
-        // an extra increase would occur as by default, at termination all files are committed.
-        // By calling this only when close sc are occurred, we guarantee the correct count of
-        // how many close sc occurs. Also, checks are computed to increase the count only if the
-        // commit count is greater than 1 to avoid unnecessary overhead.
+        /**
+         * The increase close count is called only on explicit close() sc, as defined by the
+         * CAPIO-CL specification. If it were to be called every time the file is committed, then
+         * an extra increase would occur as by default, at termination all files are committed.
+         * By calling this only when close sc are occurred, we guarantee the correct count of
+         * how many close sc occurs. Also, checks are computed to increase the count only if the
+         * commit count is greater than 1 to avoid unnecessary overhead.
+         */
         if (capio_cl_engine->getCommitCloseCount(filename) > 1) {
             CapioFileManager::increaseCloseCount(path);
         }
