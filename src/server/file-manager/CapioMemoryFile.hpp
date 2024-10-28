da3aa3f30665da62ef122b7282f854e25343bff2
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2024-10-28 11:24:57 +0100

    Write operation inside CapioMemFile

--- src/server/file-manager/CapioMemoryFile.hpp
+++ src/server/file-manager/CapioMemoryFile.hpp
@@ -12,19 +12,63 @@ class CapioMemoryFile {
     const std::string fileName;
     std::size_t totalSize;
 
+    // maps for bits
+    static constexpr u_int32_t _pageSizeMB    = 4;
+    static constexpr u_int64_t _pageMask      = 0xFFFFF;
+    static constexpr u_int64_t _pageSizeBytes = _pageSizeMB * 1024 * 1024;
+
   public:
     explicit CapioMemoryFile(const std::string &filePath) : fileName(filePath), totalSize(0) {}
 
-    void writeData(const std::size_t offset, const char *buffer, const std::size_t length) {
+    /**
+     * Write data to a file stored inside the memory
+     * @param offset offset internal to the file
+     * @param buffer buffer to store iside memory (i.e. content of the file)
+     * @param length Size of the buffer.
+     */
+    void writeData(const std::size_t offset, const char *buffer, std::size_t length) {
 
-        // Find or create the memory block for the specified offset
-        std::vector<char> &block = memoryBlocks[offset];
-        block.resize(length); // Resize the block to hold the new data
+        // Compute the offset of the memoryBlocks component.
+        // This is done by first obtaining the MB component of the address
+        // and then dividing it by the size in megabyte of the address
+        const auto map_offset = (offset >> 20) / _pageSizeMB;
 
-        // Copy the data into the block
-        std::copy(buffer, buffer + length, block.begin());
+        // Compute the first write offset relative to the first block of memory
+        const auto write_offset = offset & _pageMask;
 
-        // Update the total size if this write extends beyond the current end
+        // compute the first write size. if the write operation is bigger than the size of the page
+        // in bytes, then we need to perform the first write operation with size equals to the
+        // distance between the write offset and the end of the page. otherwise it is possible to
+        // use the given length
+        const auto first_write_size =
+            length > _pageSizeBytes ? _pageSizeBytes - write_offset : length;
+
+        // Execute first write which could be a smaller size
+        auto &block = memoryBlocks[map_offset];
+        block.resize(_pageSizeBytes); // reserve 4MB of space
+        std::copy_n(buffer, first_write_size, block.begin() + write_offset);
+
+        // update remaining bytes to write
+        length -= first_write_size;
+        size_t map_count = 1; // start from map following the one obtained from the first write
+
+        // Variable to store the read offset of the input buffer
+        auto buffer_offset = first_write_size;
+
+        while (length > 0) {
+            block = memoryBlocks[map_offset + map_count];
+            block.resize(_pageSizeBytes);
+
+            // Compute the actual size of the current write
+            const auto write_size = length > _pageSizeBytes ? _pageSizeBytes : length;
+
+            std::copy_n(buffer + buffer_offset, write_size, block.begin());
+
+            buffer_offset += write_size;
+            map_count++;
+            length -= _pageSizeBytes;
+        }
+
         totalSize = std::max(totalSize, offset + length);
     }
 
