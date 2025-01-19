79f624afb0045824b2d156045ae89904da90010a
Gabriele Calastri <calastrigabriele@gmail.com>
2025-01-19 20:25:00 +0100

    test start

--- tests/unit/server/src/TestCommService.hpp
+++ tests/unit/server/src/TestCommService.hpp
@@ -2,25 +2,48 @@
 #define TEST_CAPIOCOMMUNICATIONSERVICE_HPP
 
 #include "../server/communication-service/CapioCommunicationService.hpp"
+#include <thread>
+#include <gtest/gtest.h>
+void startFirst() {
+    char recvBuff[1024];
+    CapioCommunicationService first("1234");
+    std::string receivedHostname = first.recive(recvBuff, 1024);
+    EXPECT_STREQ(recvBuff, "ping");
+    EXPECT_EQ(receivedHostname, "localhost");
+}
+
+void startSecond() {
+    CapioCommunicationService second("1234");
+    char buff[5]{'p', 'i', 'n', 'g', '\0'};
+    second.send("fd-01", buff, sizeof(buff));
+}
+
+class CapioCommServiceTest : public ::testing::Test {
+
+    std::thread frist_thread_;
+    std::thread second_thread;
+
+    void SetUp() override {
+        frist_thread_ = std::thread(startFirst);
+        std::this_thread::sleep_for(std::chrono::seconds(1));// aspetta che server parta
+
+    }
+
+    void TearDown() override {
+        if (second_thread.joinable()) {
+            second_thread.join(); //join with main and let main continue
+            // (unisci il tread con il main e fai continuare il main)
+        }
+        if (frist_thread_.joinable()) { //jonable vuol dire che il thread ha finito e sta aspettando
+                                            //di riunirsi con il main
+            second_thread.join();
+        }
+    }
+};
 
 TEST(CapioCommServiceTest, TestNumberOne) {
 
-   std::string port = "1234";
-    CapioCommunicationService CapioCommunicationService(port); // hostanmes non serve
-    uint64_t NumberBytes = 50;
-    EXPECT_EQ();
-    // parte del codice di chi vuole ricevere
-    /*
 
-           char buff[NumberBytes];
-           std::string  HostnameFrom = tcp.recive( buff, NumberBytes);
-           std::cout << "il server ha ricevuto da "<< HostnameFrom << ": "<< buff << "\n" ;
-    */
-
-    // parte del codice di chi vuole mandare
-   /* char buff[5]{'p', 'i', 'n', 'g', '\0'};
-    std::string ReceiverHostname = "fd-01"; // conosco l'hostname della persona a cui volgio mandare
-    CapioCommunicationService.send(ReceiverHostname, buff, NumberBytes);*/
 }
 
 #endif // TEST_CAPIOCOMMUNICATIONSERVICE_HPP
