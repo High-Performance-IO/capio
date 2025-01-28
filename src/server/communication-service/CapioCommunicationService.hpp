21c532ea6e4c66440a05f13f762fbbbb87dd8f8f
Gabriele Calastri <calastrigabriele@gmail.com>
2025-01-28 14:12:14 +0100

    test send recv 1

--- src/server/communication-service/CapioCommunicationService.hpp
+++ src/server/communication-service/CapioCommunicationService.hpp
@@ -73,9 +73,9 @@ class CapioCommunicationService : BackendInterface { //CapioCommunicationService
             std::ifstream MyReadFile(entry.path().filename()); // apri file
             std::string TryHostName = entry.path().stem();
             std::string TryPort;
-            LOG(entry.path().filename().c_str());
-            LOG(MyToken.c_str());
-            LOG(TryHostName.c_str());
+           // LOG(entry.path().filename().c_str());
+           // LOG(MyToken.c_str());
+           // LOG(TryHostName.c_str());
 
 
             while (getline(MyReadFile, TryPort)) { // SALVA PORTA
