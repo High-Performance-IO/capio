18c419051019371d73288d224cf512d3082ed592
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-09-11 13:36:18 +0200

    Implemented one function in CapioRemoteFile. other functions throws exceptions

--- capio-server/src/storage-service/capio_remote_file.cpp
+++ capio-server/src/storage-service/capio_remote_file.cpp
@@ -11,17 +11,28 @@ CapioRemoteFile::~CapioRemoteFile() {}
 
 std::size_t CapioRemoteFile::writeData(const char *buffer, const std::size_t file_offset,
                                        std::size_t buffer_length) {
-    return 0;
+    throw std::runtime_error("Not implemented: writeData");
 }
 
 std::size_t CapioRemoteFile::readData(char *buffer, std::size_t file_offset,
                                       std::size_t buffer_size) {
-    return 0;
+    throw std::runtime_error("Not implemented: readData");
 }
 
-void CapioRemoteFile::readFromQueue(SPSCQueue &queue, std::size_t offset, std::size_t length) {}
+void CapioRemoteFile::readFromQueue(SPSCQueue &queue, std::size_t offset, std::size_t length) {
+    throw std::runtime_error("Not implemented: readFromQueue");
+}
 
 std::size_t CapioRemoteFile::writeToQueue(SPSCQueue &queue, std::size_t offset,
                                           std::size_t length) const {
-    return 0;
+    auto buffer = new char[length];
+
+    auto buffer_size =
+        capio_backend->fetchFromRemoteHost(this->fileName, this->homeNode, buffer, offset, length);
+
+    queue.write(buffer, buffer_size);
+
+    delete[] buffer;
+
+    return buffer_size;
 }
