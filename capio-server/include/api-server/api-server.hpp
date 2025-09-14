f30d78ad0e560fdb8817fd1450c7d5dbb43a3cd2
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-09-14 10:54:59 +0200

    Improved api-server

--- capio-server/include/api-server/api-server.hpp
+++ capio-server/include/api-server/api-server.hpp
@@ -1,14 +1,21 @@
 #ifndef CAPIO_API_SERVER_HPP
 #define CAPIO_API_SERVER_HPP
 #include <httplib.h>
+#include <string>
 #include <thread>
+#include <unordered_map>
+
 class CapioAPIServer {
+    typedef std::unordered_map<std::string, std::string> ResponseMap;
+
     std::thread *th;
     httplib::Server svr;
     static void api_server_main_func(int server_port, httplib::Server *svr);
+    static std::string
+    build_json_response(std::unordered_map<std::string, std::string> &response_map);
 
   public:
-    CapioAPIServer(int server_port);
+    explicit CapioAPIServer(int server_port);
     ~CapioAPIServer();
 };
 
--- capio-server/src/api-server/api-server.cpp
+++ capio-server/src/api-server/api-server.cpp
@@ -1,8 +1,29 @@
-#include "capio/constants.hpp"
-#include "include/client-manager/client_manager.hpp"
-#include "include/utils/configuration.hpp"
-
+#include <capio/constants.hpp>
 #include <include/api-server/api-server.hpp>
+#include <include/client-manager/client_manager.hpp>
+#include <include/utils/configuration.hpp>
+
+std::string
+CapioAPIServer::build_json_response(std::unordered_map<std::string, std::string> &response_map) {
+
+    response_map["hostname"] = capio_global_configuration->node_name;
+    response_map["wf_name"]  = capio_global_configuration->workflow_name;
+
+    std::string json_response = "{";
+
+    for (auto &[key, value] : response_map) {
+        json_response += "\"" + key + "\":\"";
+        json_response += value + "\",";
+    }
+
+    // Remove last comma to ensure json validity
+    if (json_response.back() == ',') {
+        json_response.pop_back();
+    }
+
+    json_response += "}";
+    return json_response;
+}
 
 void CapioAPIServer::api_server_main_func(int server_port, httplib::Server *svr) {
 
@@ -10,26 +31,34 @@ void CapioAPIServer::api_server_main_func(int server_port, httplib::Server *svr)
                    "Started API server on port: " + std::to_string(server_port));
 
     svr->Get("/", [](const httplib::Request &req, httplib::Response &res) {
-        res.set_content("{\"hostname\" : \"" +
-                            std::string(capio_global_configuration->node_name) +
-                            "\",\"wf_name\" : \"" + capio_global_configuration->workflow_name +
-                            "\"}",
-                        "application/json");
+        ResponseMap map;
+        res.set_content(build_json_response(map).c_str(), "application/json");
     });
 
     svr->Get("/clients", [](const httplib::Request &req, httplib::Response &res) {
-        res.set_content("{\"connected_clients\":\"" +
-                            std::to_string(client_manager->get_connected_posix_client()) + "\"}",
-                        "application/json");
+        ResponseMap map;
+        map["connected_clients"] = std::to_string(client_manager->get_connected_posix_client());
+
+        res.set_content(build_json_response(map).c_str(), "application/json");
     });
 
     svr->Get("/terminate", [](const httplib::Request &req, httplib::Response &res) {
         server_println(CAPIO_SERVER_CLI_LOG_SERVER_WARNING,
                        "Received shutdown request from API Server");
         capio_global_configuration->termination_phase = true;
-        res.set_content("{\"status\":\"shutdown\"}", "application/json");
+        ResponseMap map;
+        map["status"] = "shutting-down";
+        res.set_content(build_json_response(map).c_str(), "application/json");
         kill(capio_global_configuration->CAPIO_SERVER_MAIN_PID,
              SIGUSR1); // Wake parent child and children
+    });
+
+    svr->set_error_handler([](const httplib::Request &req, httplib::Response &res) {
+        ResponseMap map;
+
+        map["status"]  = std::to_string(res.status);
+        map["message"] = "Error: Unknown request: " + req.path;
+        res.set_content(build_json_response(map).c_str(), "application/json");
     });
 
     svr->listen("*", server_port);
