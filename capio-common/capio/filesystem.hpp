0f510b7d559183e33a0d3dc3d9f06be3ff907183
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-09-15 12:27:02 +0200

    Added check for forbidden path in is_capio_path

--- capio-common/capio/filesystem.hpp
+++ capio-common/capio/filesystem.hpp
@@ -73,7 +73,8 @@ inline bool is_capio_path(const std::filesystem::path &path_to_check) {
     START_LOG(capio_syscall(SYS_gettid), "call(path_to_check=%s)", path_to_check.c_str());
 
     // check if path_to_check begins with CAPIO_DIR
-    const auto res = is_prefix(get_capio_dir(), path_to_check);
+    const auto res =
+        is_prefix(get_capio_dir(), path_to_check) && !is_forbidden_path(path_to_check.string());
     LOG("is_capio_path:%s", res ? "yes" : "no");
     return res;
 }
