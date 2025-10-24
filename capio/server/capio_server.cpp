99ea625b2820b6d31705090e37b0ee7a2498d891
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-10-24 09:55:24 +0200

    Finalized class and made method inline

--- capio/server/capio_server.cpp
+++ capio/server/capio_server.cpp
@@ -20,10 +20,10 @@
 
 #include "capiocl.hpp"
 /// @brief const wrapper to class instance of capiocl::Engine
-class CapioCLEngine {
+class CapioCLEngine final {
   public:
     /// @brief Get a const reference to capiocl::Engine instance
-    const static capiocl::Engine &get();
+    inline const static capiocl::Engine &get();
 };
 
 std::string workflow_name;
