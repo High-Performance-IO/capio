0c5f44f5ab192daab49315cb2593eb1b4da28ce8
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-12-06 13:56:11 +0100

    Moved from condition variable to loop checking for presence

--- capio/posix/utils/clone.hpp
+++ capio/posix/utils/clone.hpp
@@ -1,14 +1,15 @@
 #ifndef CAPIO_POSIX_UTILS_CLONE_HPP
 #define CAPIO_POSIX_UTILS_CLONE_HPP
+#include <chrono>
 #include <condition_variable>
+#include <thread>
 
 #include "common/syscall.hpp"
 #include "data.hpp"
 #include "requests.hpp"
 
 inline std::mutex mutex_child;
-inline std::condition_variable child_continue_execution;
-inline std::vector<long> initialized_children;
+std::vector<long> initialized_children;
 
 /**
  * Initialize the required data structures for the new child thread, and then proceed to execute a
@@ -40,31 +41,33 @@ inline void initialize_new_thread() {
 inline void hook_clone_child() {
     START_LOG(capio_syscall(SYS_gettid), "call()");
     std::unique_lock ul(mutex_child);
-    child_continue_execution.wait(ul, []() {
-        const long tid = syscall_no_intercept(SYS_gettid);
-        return std::find(initialized_children.begin(), initialized_children.end(), tid) !=
-               initialized_children.end();
-    });
+    const long tid            = syscall_no_intercept(SYS_gettid);
+    bool found_thread         = false;
+    syscall_no_intercept_flag = true;
+    do {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::lock_guard lock(mutex_child);
+        found_thread = std::find(initialized_children.begin(), initialized_children.end(), tid) !=
+                       initialized_children.end();
+    } while (!found_thread);
+    syscall_no_intercept_flag = false;
+
     LOG("Parent unlocked thread");
     initialize_new_thread();
 }
 
 /**
  * Routine given to syscall_intercept to handle the execution of a clone syscall. After issuing a
- * clone request, it increases by one the sem_post semaphore unloking the children thread.
- * @param child_pid
+ * clone request, it increases by one the sem_post semaphore unlocking the children thread.
+ * @param child_tid
  */
-inline void hook_clone_parent(const long child_pid) {
+inline void hook_clone_parent(const long child_tid) {
     const auto parent_tid = syscall_no_intercept(SYS_gettid);
-    START_LOG(parent_tid, "call(parent_tid=%d, child_pid=%d)", parent_tid, child_pid);
-    clone_request(parent_tid, child_pid);
+    START_LOG(parent_tid, "call(parent_tid=%d, child_pid=%d)", parent_tid, child_tid);
+    clone_request(parent_tid, child_tid);
 
-    {
-        std::lock_guard lg(mutex_child);
-        initialized_children.push_back(child_pid);
-    }
-
-    child_continue_execution.notify_all();
+    std::lock_guard lg(mutex_child);
+    initialized_children.emplace_back(child_tid);
 }
 
 #endif // CAPIO_POSIX_UTILS_CLONE_HPP
