9325e8154ca531b814748201e7b5d2c559b1c26b
= <=>
2026-02-17 10:36:24 +0100

    Fixed bug in CapioFile class

--- capio/server/include/storage/manager.hpp
+++ capio/server/include/storage/manager.hpp
@@ -96,8 +96,10 @@ class StorageManager {
      * @param tid The thread ID.
      * @param fd The file descriptor number.
      * @param path The path of the file being opened.
+     * @param register_open Whether this operation should also call open() on the CapioFile instance
      */
-    void _addNewFdToStorage(pid_t tid, int fd, const std::filesystem::path &path);
+    void _addNewFdToStorage(pid_t tid, int fd, const std::filesystem::path &path,
+                            bool register_open);
 
   public:
     /**
