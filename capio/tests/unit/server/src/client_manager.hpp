b6fe558c0cbb077410e71f59b9492fae8272bf84
= <=>
2026-02-17 13:56:36 +0000

    tests

--- capio/tests/unit/server/src/client_manager.hpp
+++ capio/tests/unit/server/src/client_manager.hpp
@@ -1,21 +1,50 @@
 #ifndef CAPIO_CLIENT_MANAGER_HPP
 #define CAPIO_CLIENT_MANAGER_HPP
 
-char *node_name;
-std::string workflow_name;
-
-#include "capiocl.hpp"
-#include "utils/capiocl_adapter.hpp"
-capiocl::Engine capio_cl_engine;
-const capiocl::Engine &CapioCLEngine::get() { return capio_cl_engine; }
-
 #include "client-manager/client_manager.hpp"
-ClientManager *clientManager;
+
+#include <thread>
 
 TEST(clientManagerTestSuite, testReplyToNonClient) {
-    clientManager = new ClientManager();
+    ClientManager client_manager;
     char buffer[1024];
-    EXPECT_THROW(clientManager->replyToClient(-1, 0, buffer, 0), std::runtime_error);
+    EXPECT_THROW(client_manager.replyToClient(-1, 0, buffer, 0), std::runtime_error);
+}
+
+TEST(clientManagerTestSuite, testGetNumberOfConnectedClients) {
+    ClientManager client_manager;
+    EXPECT_EQ(client_manager.getConnectedPosixClients(), 0);
+
+    client_manager.registerClient(1234);
+
+    EXPECT_EQ(client_manager.getConnectedPosixClients(), 1);
+
+    client_manager.removeClient(1234);
+
+    EXPECT_EQ(client_manager.getConnectedPosixClients(), 0);
+}
+
+TEST(ClientManagerTestSuite, testFailedRequestCode) {
+
+    ClientManager client_manager;
+
+    // NOTE: there is no need to delete this object as it is only attaching to the shm allocated by
+    // client_manager. Also calling delete on this raises std::terminate as an exception is thrown
+    // in the destructor
+    // TODO: change behaviour of ERR_EXIT to not throw exceptions but only print error and continue
+    const auto request_queue =
+        new CircularBuffer<char>(SHM_COMM_CHAN_NAME, CAPIO_REQ_BUFF_CNT, CAPIO_REQ_MAX_SIZE);
+
+    char req[CAPIO_REQ_MAX_SIZE], new_req[CAPIO_REQ_MAX_SIZE];
+    sprintf(req, "123 aaaa bbbb cccc");
+    request_queue->write(req, CAPIO_REQ_MAX_SIZE);
+
+    EXPECT_EQ(client_manager.readNextRequest(new_req), 123);
+
+    sprintf(req, "abc aaaa bbbb cccc");
+    request_queue->write(req, CAPIO_REQ_MAX_SIZE);
+
+    EXPECT_EQ(client_manager.readNextRequest(new_req), -1);
 }
 
 #endif // CAPIO_CLIENT_MANAGER_HPP
--- capio/tests/unit/server/src/main.cpp
+++ capio/tests/unit/server/src/main.cpp
@@ -1,5 +1,14 @@
+#include "common/constants.hpp"
+
 #include <gtest/gtest.h>
-#include <iostream>
+
+char *node_name;
+std::string workflow_name = CAPIO_DEFAULT_WORKFLOW_NAME;
+
+#include "capiocl.hpp"
+#include "utils/capiocl_adapter.hpp"
+capiocl::Engine capio_cl_engine;
+const capiocl::Engine &CapioCLEngine::get() { return capio_cl_engine; }
 
 #include "capio_file.hpp"
 #include "client_manager.hpp"
