c488793ee64e3eba82f94f2e18961a95c28da58a
= <=>
2026-03-23 11:51:02 +0000

    test

--- capio/server/src/backend.cpp
+++ capio/server/src/backend.cpp
@@ -5,6 +5,10 @@ Backend::Backend(const unsigned int node_name_max_length)
     gethostname(node_name.data(), node_name_max_length);
 }
 
-[[nodiscard]] const std::string &Backend::get_node_name() const { return node_name; }
+[[nodiscard]] const std::string &Backend::get_node_name() const {
+    START_LOG(gettid(), "call()");
+    LOG("THIS node_name = %s", node_name.c_str());
+    return node_name;
+}
 
 std::set<std::string> Backend::get_nodes() { return {node_name}; }
