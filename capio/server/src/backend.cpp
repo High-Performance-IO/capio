5a47f94df0eeaf7f921800d8cf11447c93924238
= <=>
2026-03-23 12:15:47 +0000

    fix

--- capio/server/src/backend.cpp
+++ capio/server/src/backend.cpp
@@ -1,8 +1,10 @@
 #include "remote/backend.hpp"
 
-Backend::Backend(const unsigned int node_name_max_length)
-    : n_servers(1), node_name(node_name_max_length, '\0') {
-    gethostname(node_name.data(), node_name_max_length);
+Backend::Backend(const unsigned int node_name_max_length) : n_servers(1) {
+    const auto node_name_tmp = new char[node_name_max_length]{0};
+    gethostname(node_name_tmp, node_name_max_length);
+    node_name = node_name_tmp;
+    delete[] node_name_tmp;
 }
 
 [[nodiscard]] const std::string &Backend::get_node_name() const {
