9c8abbf592cd4caf0411a3972f36d436f8960f74
marcoSanti <marcoedoardo.santimaria@unito.it>
2026-04-27 11:41:05 +0200

    Cleanup and comments

--- capio/server/include/remote/discovery.hpp
+++ capio/server/include/remote/discovery.hpp
@@ -6,6 +6,13 @@
 
 #include "utils/shm_canary.hpp"
 
+/**
+ * Discovery service. Responsible for:
+ * - Detect other server instances running in the same node with the same workflow name (and halts
+ * startup if it finds one)
+ * - Detect other remote running server instances of capio servers and issue commands to the backend
+ * to open a connection with them as soon as they are found.
+ */
 class DiscoveryService {
 
     /// @brief Variable used to signal termination to child threads
@@ -25,34 +32,48 @@ class DiscoveryService {
     /// equivalent to the one starting up
     CapioShmCanary *shm_canary;
 
-    std::filesystem::path token_directory_path = ".capio_tokens/";
+    /// @brief Directory to look into for CAPIO tokens
+    std::filesystem::path token_directory_path;
+    /// @brief This server instance token filename
     std::filesystem::path token_filename;
 
+    /// @brief Multicast address
+    const std::string capio_multicast_adv_address;
+
+    /// @brief multicast port
+    const unsigned int capio_multicast_adv_port;
+
   public:
-    /// @brief Default constructor
-    DiscoveryService();
+    /**
+     * Construct a new Discovery Service class
+     * @param mcast_addr Address to send and receive aliveness token from other servers
+     * @param mcast_port Port to send and receive aliveness token from other servers
+     */
+    explicit DiscoveryService(const std::string &mcast_addr = CAPIO_MCAST_ADV_DEFAULT_ADDR,
+                              unsigned int mcast_port       = CAPIO_MCAST_ADV_DEFAULT_PORT);
 
     /// @brief Default destructor
     ~DiscoveryService();
 
     /**
-     * Set the token to be advertised so that other server instance may connect to this instance.
-     * Token needs to be provided by an instance of a backend, according to backend specification
-     * for incoming connection.
+     * @brief Configures and starts the discovery service to advertise and scan for tokens.
      *
-     * Once the token is set, a new hidden file with the current token is stored within a hidden
-     * directory.
-     * @param token
+     * Sets the advertisement token used by other server instances to establish a connection.
+     * The token must conform to the specific backend requirements for incoming connections.
+     * * @note The token is not passed via the constructor because the Discovery Service
+     * must be instantiated before the Backend provides the token.
+     *
+     * Once called, this method:
+     * 1. Stores the current token in a hidden file within a designated directory.
+     * 2. Initiates multicast traffic to advertise the local token.
+     * 3. Scans the hidden directory for aliveness tokens from other servers.
+     *
+     * @param adv_delay The interval (in milliseconds/seconds) between advertisement broadcasts.
+     * @param token The authentication or identification string provided by the backend.
+     * @param token_directory directory to store capio aliveness tokens
      */
-    void setAdvertisementToken(const std::string &token);
-
-    /**
-     * Start to advertise the token, and to scan for tokens from other servers. Advertisement works
-     * by sending multicast traffic, and by scanning files contained within the hidden directory
-     * with aliveness tokens.
-     * @param adv_delay Delay between each advertisement.
-     */
-    void start(unsigned int adv_delay);
+    void start(unsigned int adv_delay, const std::string &token,
+               const std::string &token_directory = ".capio_tokens/");
 
     /**
      * Stop current server instance from advertising itself and from receiving advertisements from
