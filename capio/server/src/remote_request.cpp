6ec7db369bde6b56b3d5182703930b186552d777
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2026-08-11 17:31:00 +0200

    Test for fixes

--- capio/server/src/remote_request.cpp
+++ capio/server/src/remote_request.cpp
@@ -16,9 +16,25 @@ RemoteRequest::RemoteRequest(char *buf_recv, const std::string &source) : _sourc
     } else {
         this->_code = -1;
     }
+    if (buf_recv.size() < 5 || buf_recv[4] != ' ') {
+        return;
+    }
+
+    const auto [ptr, ec] = std::from_chars(buf_recv.data(), buf_recv.data() + 4, _code);
+    if (ec != std::errc() || ptr != buf_recv.data() + 4) {
+        _code = -1;
+        return;
+    }
+
+    _content = buf_recv.substr(5);
+    LOG("Received request %d from %s : %s", _code, _source.c_str(), _content.c_str());
 }
 
-RemoteRequest::~RemoteRequest() { delete[] _buf_recv; }
+RemoteRequest::RemoteRequest(char *buf_recv, const std::string &source)
+    : RemoteRequest(buf_recv == nullptr ? std::string{} : std::string(buf_recv), source) {
+    delete[] buf_recv;
+}
+
 const std::string &RemoteRequest::get_source() const { return this->_source; }
-[[nodiscard]] const char *RemoteRequest::get_content() const { return this->_buf_recv; }
+[[nodiscard]] const char *RemoteRequest::get_content() const { return this->_content.c_str(); }
 [[nodiscard]] int RemoteRequest::get_code() const { return this->_code; }
